e3871e94e501e338e82fbd10891aed643e79bd79
Lucas Pamplona <pamplona.developer@gmail.com>
2025-03-20 20:53:38 -0300

    Exercicio para proxima aula

--- src/pages/PostPage/index.jsx
+++ src/pages/PostPage/index.jsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 
 function PostPage() {
     const { postId } = useParams();
+    const [postInfo, setPostInfo] = useState({});
+    const [comments, setComments] = useState([]);
+
+    const getPostInfo = () => {}
+    const getComments = () => {}
+
+    useEffect(() => {
+        getPostInfo();
+        getComments();
+    }, [postId])
+
+    // fazer requisição para pegar informações do post
+    // https://jsonplaceholder.typicode.com/posts/${postId}
+    // Mostrar titulo completo do post e corpo do post
+
+    // fazer requisição para pegar comentários do post
+    // https://jsonplaceholder.typicode.com/posts/${postId}/comments
+    // mostrar comentários do post no final da página
 
     return (
         <>
